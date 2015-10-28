35e2a9a3f6b8823b00ada890ed33344fd6ac7f5b
guan <15513094814@163.com>
2015-10-28 23:09:00 +0800

    2222222222

--- src/com/atguigu/test/Test.java
+++ src/com/atguigu/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("lllllllllllll");
 		System.out.println(".................");
+		System.out.println("2222");
 	}
 
 }
